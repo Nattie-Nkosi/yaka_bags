9cfa728b0a3f062b0a442f45daaf2bc6c8063a6d
Nutty Nkosi <nkosin361@gmail.com>
2020-01-25 00:24:49 +0200

    My second Commit

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/Y.png" type="image/gif" sizes="16x16">    
+    <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/Grid.css">
+    <meta name="description" content="Yaka Bags is a premium bags delivery dervice with the mission to bring affordable and quality bags to as many people as possible.">   
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic' rel='stylesheet' type='text/css'>
+    <title>Yaka Bags</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="row">
+                <img src="/resources/img/logo2.png" alt="Yaka bags logo" class="logo">
+                <img src="/resources/img/logo1.png" alt="Yaka bags logo" class="logo-black">
+                <ul class="main-nav js--main-nav">
+                    <li><a href="#features">About Us</a></li>
+                    <li><a href="#bags">Bags</a></li>
+                    <li><a href="#location">Where To us</a></li>
+                    <li><a href="#talk">Talk To Us</a></li>
+                </ul>  
+                <a class="mobile-nav-icon js--nav-icon"><i class="ion-navicon-round"></i></a>
+            </div>
+            
+        </nav>
+        <div class="hero-text-box">
+            <h1>PREMIUM BACKPACKS, <br> WITH PERSONAL TOUCH</h1>
+            <a class="btn btn-full js--scroll-to-start" href="#">ENQUIRE NOW</a>
+        </div>
+    </header>
+
+    <section class="section-features js--section-features" id="features">
+        <div class="row">
+            <h2>Get Quality Bags &mdash; not bags</h2>
+            <p class="long-copy">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil beatae et earum voluptate.
+                 Rem accusantium optio voluptatum recusandae expedita vel reiciendis molestias minima, magni doloribus, id, voluptates quibusdam natus.
+            </p>
+        </div>
+        <div class="row js--wp-1">
+            <div class="col span-1-of-4 box">
+                <i class="ion-briefcase icon-big"></i>
+                <h3>Lorem ipsum dolor.</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae necessitatibus est esse voluptatibus fugit,
+                     consequatur dolorum fuga accusamus possimus obcaecati commodi explicabo sed. In facere labore nostrum molestias eius sit!</p>
+            </div>
+      
+            <div class="col span-1-of-4 box">
+                <i class="ion-android-cart icon-big"></i>
+                <h3>Lorem ipsum dolor.</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae necessitatibus est esse voluptatibus fugit,
+                     consequatur dolorum fuga accusamus possimus obcaecati commodi explicabo sed. In facere labore nostrum molestias eius sit!</p>
+            </div>
+        
+            <div class="col span-1-of-4 box">
+                <i class="ion-android-alarm-clock icon-big"></i>
+                <h3>Lorem ipsum dolor.</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae necessitatibus est esse voluptatibus fugit,
+                     consequatur dolorum fuga accusamus possimus obcaecati commodi explicabo sed. In facere labore nostrum molestias eius sit!</p>
+            </div>
+        
+            <div class="col span-1-of-4 box">
+                <i class="ion-android-happy icon-big"></i>
+                <h3>Lorem ipsum dolor.</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae necessitatibus est esse voluptatibus fugit,
+                     consequatur dolorum fuga accusamus possimus obcaecati commodi explicabo sed. In facere labore nostrum molestias eius sit!</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-meals" id="bags">
+        <ul class="meals-showcase">
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image1.jpeg" alt="">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image2.jpeg" alt="">
+                </figure>
+            </li>  
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image3.jpeg" alt="">
+                </figure>
+            </li>   
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image4.jpeg" alt="">
+                </figure>
+            </li> 
+        </ul>
+        <ul class="meals-showcase clearfix">
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image5.jpeg" alt="">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image6.jpeg" alt="">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image7.jpeg" alt="">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="/resources/img/image8.jpeg" alt="">
+                </figure>
+            </li>
+        </ul>
+    </section>
+
+    <section class="section-cities" id="location">
+        <div class="row js--wp-3">
+            <h2>We're currently in these cities</h2>
+        </div>
+        <div class="row">
+            <div class="col span-1-of-4">
+                <img src="resources/img/city.png" alt="Lisbon">
+                <h3>Sandton City</h3>
+
+                <div class="city-feature">
+                    <i class="ion-navigate icon-small"></i>
+                    20 Denise Road
+                </div>
+                <div class="city-feature">
+                    <i class="ion-map icon-small"></i>
+                    Buccleuch
+                </div>
+                <div class="city-feature">
+                    <i class="ion-location icon-small"></i>
+                    Sandton
+                </div>
+                <div class="city-feature">
+                    <i class="ion-pin icon-small"></i>
+                    2090
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-testimonials">
+        <div class="row">
+            <h2>Our Customers Can't live without us</h2>
+        </div>
+        <div class="row">
+            <div class="col span-1-of-3">
+                <blockquote>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque saepe quasi minima corporis, pariatur autem architecto quas facere hic et!
+                    <cite>Romeo Nkosi</cite>
+                </blockquote>
+            </div>
+            <div class="col span-1-of-3">
+                <blockquote>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque saepe quasi minima corporis, pariatur autem architecto quas facere hic et!
+                    <cite>Sivuyile Pezulu</cite>
+                </blockquote>
+            </div>
+            <div class="col span-1-of-3">
+                <blockquote>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque saepe quasi minima corporis, pariatur autem architecto quas facere hic et!
+                    <cite>Nutty Nkosi</cite>
+                </blockquote>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-form js--section-form" id="talk">
+        <div class="row">
+            <h2>We're happy to hear from you</h2>
+        </div>
+        <div class="row">
+            <?php 
+                if($_GET["success"] == 1){
+                    echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
+                }
+                
+                if($_GET["success"] == -1){
+                    echo "<div class=\"form-messages error\">Oops! Something went wrong. Please try again.</div>";
+                }
+            ?>
+        </div>
+        <div class="row">
+            <form method="post" action="mailer.php" class="contact-form">
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label for="name">Name</label>
+                    </div>  
+                    <div class="col span-2-of-3">
+                        <input type="text" name="name" id="name" placeholder="your name" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label for="email">Email</label>
+                    </div>  
+                    <div class="col span-2-of-3">
+                        <input type="email" name="email" id="email" placeholder="your email" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label for="find-us">Bag Color</label>
+                    </div>  
+                    <div class="col span-2-of-3">
+                       <select name="find-us" id="find-us">
+                           <option value="blue">Blue</option>
+                           <option value="green">Green</option>
+                           <option value="pink">Pink</option>
+                           <option value="rainbow">Rainbow Colors</option>
+                       </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label for="message">Drop a line</label>
+                    </div>  
+                    <div class="col span-2-of-3">
+                        <textarea name="message" placeholder="Your message"></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col span-1-of-3">
+                        <label for="">&nbsp;</label>
+                    </div>  
+                    <div class="col span-2-of-3">
+                        <input type="submit" value="Sent it">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <div class="row">
+            <div class="col span-1-of-2">
+                <div class="footer-nav">
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Bags</a></li>
+                    <li><a href="#">Where to find us</a></li>
+                    <li><a href="#">Talk to us</a></li>
+                </div>
+            </div>
+            <div class="col span-1-of-2">
+                <ul class="social-links">
+                    <li><a href="https://www.instagram.com/yaka.bags/"><i class="ion-social-instagram"></i></a></li>
+                    <li><a href="https://www.twitter.com/yaka_bags/"><i class="ion-social-twitter"></i></a></li>
+                </ul>
+            </div>
+            <div class="row">
+                <p>
+                    Copyright &copy; 2020 by Yaka Bags. All rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.jsdelivr.net/selectivizr/1.0.3b/selectivizr.min.js"></script>
+    <script src="/vendors/js/jquery.waypoints.min.js"></script>
+    <script src="/resources/js/script.js"></script>
+</body>
+</html>
